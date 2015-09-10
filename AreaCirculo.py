6d0e5b7af8c6bf285189f733cc953833c7d4f709
talivaro <taniavargas25@hotmail.com>
2015-09-10 18:37:23 -0500

    Update AreaCirculo.py

--- AreaCirculo.py
+++ AreaCirculo.py
@@ -1,5 +1,4 @@
 __author__ = 'Talivaro'
-
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,9 +8,9 @@ def randux(ent):
     xo=101
     da1=[]
     for i in range(ent):
-        mod =(17*xo+73)%127
+        mod =(17*xo+73)%487
         #print ("x",i+1,mod)
-        ui=float(mod)/127
+        ui=float(mod)/487
         #print ("U",i+1,ui)
         xo=mod
         da1.append(ui)
@@ -21,9 +20,9 @@ def randuy(ent):
     xo=23
     da2=[]
     for i in range(ent):
-        mod =(41*xo+19)%269
+        mod =(89*xo+19)%829
         #print ("x",i+1,mod)
-        ui=float(mod)/269
+        ui=float(mod)/829
         #print ("U",i+1,ui)
         xo=mod
         da2.append(ui)
@@ -49,16 +48,15 @@ for i in range(val):
     #sali[i]=(da1[i]*da1[i])+(da2[i]*da2[i])
     if(6**2>=((da1[i]*da1[i])+(da2[i]*da2[i]))):#Esta dentro del circulo?
         c=c+1
-        d=1/(N*100)
-        d=d+d
         axi.plot(da1[i],da2[i],'g.')
         #print(sali,"1")
     else:
         #print(sali,"0")
         axi.plot(da1[i],da2[i],'r.')
-    if i%120==0:
+    if i%70==0:
         plt.draw()
-        plt.title("Area del circulo"+str(math.pi*25))
-
-aestimada=c*d
-print("Area Teorica:",math.pi*25, aestimada,c)
+        plt.title("Area Teorica "+str(math.pi*25)+"Area Aproximada "+str((float(c)/val)*(100)))
+acirculo=math.pi*25
+aestimada=(float(c)/val)*(100)
+errorRelPorcen=(abs(acirculo-aestimada)/acirculo)*100
+print("Area Teorica:",math.pi*25,"Area estimada:", aestimada,"error: ",errorRelPorcen)
