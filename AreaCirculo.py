c03b8998ee9705adcbc6419e27a05524cfeb4591
talivaro <taniavargas25@hotmail.com>
2015-10-08 14:30:30 -0500

    Revert "Update AreaCirculo.py"
    This reverts commit 6d0e5b7af8c6bf285189f733cc953833c7d4f709.

--- AreaCirculo.py
+++ AreaCirculo.py
@@ -1,4 +1,5 @@
 __author__ = 'Talivaro'
+
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,9 +9,9 @@ def randux(ent):
     xo=101
     da1=[]
     for i in range(ent):
-        mod =(17*xo+73)%487
+        mod =(17*xo+73)%127
         #print ("x",i+1,mod)
-        ui=float(mod)/487
+        ui=float(mod)/127
         #print ("U",i+1,ui)
         xo=mod
         da1.append(ui)
@@ -20,9 +21,9 @@ def randuy(ent):
     xo=23
     da2=[]
     for i in range(ent):
-        mod =(89*xo+19)%829
+        mod =(41*xo+19)%269
         #print ("x",i+1,mod)
-        ui=float(mod)/829
+        ui=float(mod)/269
         #print ("U",i+1,ui)
         xo=mod
         da2.append(ui)
@@ -48,15 +49,16 @@ for i in range(val):
     #sali[i]=(da1[i]*da1[i])+(da2[i]*da2[i])
     if(6**2>=((da1[i]*da1[i])+(da2[i]*da2[i]))):#Esta dentro del circulo?
         c=c+1
+        d=1/(N*100)
+        d=d+d
         axi.plot(da1[i],da2[i],'g.')
         #print(sali,"1")
     else:
         #print(sali,"0")
         axi.plot(da1[i],da2[i],'r.')
-    if i%70==0:
+    if i%120==0:
         plt.draw()
-        plt.title("Area Teorica "+str(math.pi*25)+"Area Aproximada "+str((float(c)/val)*(100)))
-acirculo=math.pi*25
-aestimada=(float(c)/val)*(100)
-errorRelPorcen=(abs(acirculo-aestimada)/acirculo)*100
-print("Area Teorica:",math.pi*25,"Area estimada:", aestimada,"error: ",errorRelPorcen)
+        plt.title("Area del circulo"+str(math.pi*25))
+
+aestimada=c*d
+print("Area Teorica:",math.pi*25, aestimada,c)
